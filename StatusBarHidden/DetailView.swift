182c2f32bdfdc0b79f1fcc81c76f48ed36e7f3b9
nikola knezevic <nikolaknezev@gmail.com>
2024-11-13 08:53:37 +0000

    innitial commit

---
+++ StatusBarHidden/DetailView.swift
@@ -0,0 +1,26 @@
+//
+//  DetailView.swift
+//  StatusBarHidden
+//
+//  Created by Nikola Knezevic on 13/11/2024.
+//
+
+import SwiftUI
+
+struct DetailView: View {
+    var body: some View {
+        VStack {
+            
+            
+            Text("Detailed View")
+            Text("Detailed View")
+            Text("Detailed View")
+            Text("Detailed View")
+        }
+        .statusBarHidden()
+    }
+}
+
+#Preview {
+    DetailView()
+}
